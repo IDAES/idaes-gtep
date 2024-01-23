9df2b08d0b0bbc52c9bb23f80ca45833d9c5ba5c
Kyle Skolfield <jkskolf@sandia.gov>
2024-01-23 11:30:07 -0700

    Black formatting

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 import os
 from setuptools import setup, find_packages
 
-setup(name="gtep", version="0.1.dev0", python_requires=">=3.7", description="a python package", packages=find_packages())
+setup(
+    name="gtep",
+    version="0.1.dev0",
+    python_requires=">=3.7",
+    description="a python package",
+    packages=find_packages(),
+)
