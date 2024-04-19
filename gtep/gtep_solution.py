03bbe4f1207ebb529a2dd85b05dea4a82f585cb8
Thom R. Edwards <tredwar@sandia.gov>
2024-04-19 16:49:50 -0600

    fixed json dump

--- gtep/gtep_solution.py
+++ gtep/gtep_solution.py
@@ -9,6 +9,9 @@ from pyomo.environ import units as u
 from gtep.gtep_model import ExpansionPlanningModel
 import logging
 
+import json
+from pathlib import Path
+
 logger = logging.getLogger(__name__)
 
 
@@ -30,14 +33,78 @@ class ExpansionPlanningSolution:
             raise ValueError(
                 "ExpansionPlanningSolution objects loaded from model must have a results component."
             )
-        self.results = gtep_model.results
-        self.stages = gtep_model.stages
-        self.formulation = gtep_model.formulation
-        self.data = gtep_model.data
-        self.num_reps = gtep_model.num_reps
-        self.len_reps = gtep_model.len_reps
-        self.num_commit = gtep_model.num_commit
-        self.num_dispatch = gtep_model.num_dispatch
+        self.results = gtep_model.results  # Highs results object
+        self.stages = gtep_model.stages  # int
+        self.formulation = gtep_model.formulation  # None (???)
+        self.data = gtep_model.data  # ModelData object
+        self.num_reps = gtep_model.num_reps  # int
+        self.len_reps = gtep_model.len_reps  # int
+        self.num_commit = gtep_model.num_commit  # int
+        self.num_dispatch = gtep_model.num_dispatch  # int
 
-    def dump_json(self):
         pass
+
+    def dump_json(self, filename="./gtep_solution.json"):
+
+        dump_filepath = Path(filename)
+        with open(dump_filepath, "w") as fobj:
+            json.dump(self._to_dict(), fobj)
+
+    def _to_dict(self):
+
+        results_dict = {
+            "solution_loader": self.results.solution_loader,  # object
+            "termination_condition": self.results.termination_condition,  # object
+            "best_feasible_objective": self.results.best_feasible_objective,
+            "best_objective_bound": self.results.best_objective_bound,
+            "wallclock_time": self.results.wallclock_time,
+        }
+
+        # "best_feasible_objective", "best_objective_bound", and "wallclock_time" are all numbers, dont need subhandlers
+
+        # subhandle "termination_condition"
+        results_dict["termination_condition"] = {
+            "value": self.results.termination_condition.value,
+            "name": self.results.termination_condition.name,
+        }
+
+        # subhandle "solution_loader"
+        results_dict["solution_loader"] = {"primals": {}}
+
+        for key, val in self.results.solution_loader.get_primals()._dict.items():
+            results_dict["solution_loader"]["primals"][key] = {
+                "name": val[0].name,
+                "upper": val[0].upper,
+                "value": val[0].value,
+                "bounds": val[0].bounds,
+            }
+
+        # renest "termination_condition" as a json-friendly dictionary
+        # things are either vars (which have some sort of signifier in [] brackets) or are an attribute, which dont
+        # the name variable will give it away
+        results_dict["primals_tree"] = {}
+
+        for key, val in self.results.solution_loader.get_primals()._dict.items():
+            # split the name to figure out depth
+            split_name = val[0].name.split(".")
+
+            # start at the bottom and nest accordingly
+            tmp_dict = {
+                "name": val[0].name,
+                "upper": val[0].upper,
+                "value": val[0].value,
+                "bounds": val[0].bounds,
+            }
+
+            # allocate the nested dictionary
+            def nested_set(this_dict, key, val):
+                if len(key) > 1:
+                    this_dict.setdefault(key[0], {})
+                    nested_set(this_dict[key[0]], key[1:], val)
+                else:
+                    this_dict[key[0]] = val
+
+            nested_set(results_dict["primals_tree"], split_name, tmp_dict)
+        out_dict = {"data": self.data.data, "results": results_dict}
+
+        return out_dict
