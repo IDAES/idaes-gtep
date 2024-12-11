3440691e0e6cb5a1c7a220a905312aaaa9dbd27d
blnicho <blnicho@sandia.gov>
2024-12-11 15:16:41 -0700

    Fixing typo

--- gtep/gtep_solution.py
+++ gtep/gtep_solution.py
@@ -465,7 +465,7 @@ class ExpansionPlanningSolution:
         for axline_ix in range(total_height):
             ax_bins.axhline(
                 axline_ix + 0.5, color="grey", linewidth=3
-            )  # draw a seperator line between each level
+            )  # draw a separator line between each level
         for axline_ix in range(len(df[level_key])):
             ax_bins.axvline(
                 axline_ix + 0.5,
@@ -473,7 +473,7 @@ class ExpansionPlanningSolution:
                 linewidth=3,
                 linestyle="dotted",
                 alpha=0.5,
-            )  # draw a seperator line between each level
+            )  # draw a separator line between each level
 
         for ix_key, this_koi in enumerate(keys):
             # make a dummy line to steal the color cycler and make a single item for the legend
