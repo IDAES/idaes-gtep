aaf7526b2c12418e076d325ca05ede97f9e49efe
Kyle Skolfield <jkskolf@sandia.gov>
2025-07-10 10:53:37 -0600

    uhoh

--- gtep/gtep_data.py
+++ gtep/gtep_data.py
@@ -27,7 +27,7 @@ class ExpansionPlanningData:
         options_dict = {
             "data_path": data_path,
             "input_format": "rts-gmlc",
-            "start_date": "01-01-2020",
+            "start_date": "01-01-2019",
             "num_days": 365,
             "sced_horizon": 1,
             "sced_frequency_minutes": 60,
@@ -39,7 +39,7 @@ class ExpansionPlanningData:
         # Use prescient data provider to load in sequential data for representative periods
         data_list = []
 
-        x = datetime.datetime(2020, 1, 1)
+        x = datetime.datetime(2019, 1, 1)
         data_provider = gmlc_data_provider.GmlcDataProvider(options=prescient_options)
         # populate an egret model data with the basic stuff
         self.md = data_provider.get_initial_actuals_model(
@@ -75,10 +75,10 @@ class ExpansionPlanningData:
 
         time_keys = self.md.data["system"]["time_keys"]
         self.representative_dates = [
-            "2020-01-28 00:00",
-            "2020-04-23 00:00",
-            "2020-07-05 00:00",
-            "2020-10-14 00:00",
+            "2019-01-28 00:00",
+            "2019-04-23 00:00",
+            "2019-07-05 00:00",
+            "2019-10-14 00:00",
         ]   
 
         ## FIXME:
