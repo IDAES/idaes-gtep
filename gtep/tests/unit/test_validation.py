e606db68a6be77181f9140b55d09d26343f3b705
Allen Moore <agmoore@sandia.gov>
2026-02-16 16:34:07 -0500

    adding asserts to check contents of output csvs

--- gtep/tests/unit/test_validation.py
+++ gtep/tests/unit/test_validation.py
@@ -11,13 +11,17 @@
 # for full copyright and license information.
 #################################################################################
 
-from os.path import abspath, join, dirname
+from os.path import abspath, join, dirname, exists
 import pyomo.common.unittest as unittest
 from gtep.gtep_model import ExpansionPlanningModel
 from gtep.gtep_data import ExpansionPlanningData
 from gtep.gtep_solution import ExpansionPlanningSolution
 from pyomo.core import TransformationFactory
 from pyomo.contrib.appsi.solvers.highs import Highs
+
+import pandas as pd
+import pandas.testing as pdt
+from os import listdir
 
 from gtep.validation import (
     clone_timeseries,
@@ -38,8 +42,8 @@ curr_dir = dirname(abspath(__file__))
 # ...just 5 bus for now.
 data_paths_dict = {
     abspath(join(curr_dir, "..", "..", "data", "5bus")): {
-        'output': abspath(join(curr_dir, "..", "..", "data", "5bus", "out")),
-        'reference': abspath(join(curr_dir, "..", "..", "data", "5bus", "ref"))
+        'output': abspath(join(curr_dir, "..", "..", "data", "testing", "5bus", "out")),
+        'reference': abspath(join(curr_dir, "..", "..", "data", "testing", "5bus", "ref"))
     },
 }
 
@@ -52,15 +56,15 @@ gtep_model_args_list = [
         'num_dispatch': 4,
      },
     {
-        'stages': 1,
-        'num_reps': 2,
-        'len_reps': 1,
-        'num_commit': 6,
-        'num_dispatch': 4,
+        'stages': 3,
+        'num_reps': 3,
+        'len_reps': 2,
+        'num_commit': 7,
+        'num_dispatch': 5,
      }
 ]
 
-# helper function
+# helper functions
 def get_solution_objects() -> dict[str, list[ExpansionPlanningSolution]]:
     """
     :return: Dictionary that maps each data source path to a list of solution objects
@@ -92,6 +96,26 @@ def get_solution_objects() -> dict[str, list[ExpansionPlanningSolution]]:
 
     return solution_objects_dict
 
+def read_pandas_frames_and_assert_equal(path_1, path_2):
+    """
+    Read in two csvs as `pandas.DataFrame` objects and assert they
+    are equal using `pandas.testing.assert_frame_equal`.
+
+    All the asserts happen in this function.
+
+    :param path_1:
+    :param path_2:
+    :type path_1: str
+    :type path_2: str
+    """
+    assert exists(path_1)
+    assert exists(path_2)
+    out_gen_df = pd.read_csv(path_1)
+    ref_gen_df = pd.read_csv(path_2)
+
+    # TODO: check if any options should be used here
+    pdt.assert_frame_equal(out_gen_df, ref_gen_df)
+
 
 class TestValidation(unittest.TestCase):
 
@@ -104,16 +128,18 @@ class TestValidation(unittest.TestCase):
         Loops over every combination of data source and options,
         calling `func` each time.
         
-        :param func: Must accept three positional arguments:
-            data input path, solution object, and data output path
+        :param func: Must accept four positional arguments:
+            data input path, solution object, data output path, and data output reference path
         :type func: function
         """
         for data_input_path, solutions in self.solution_dict.items():
 
             out_path = data_paths_dict[data_input_path]['output']
+            ref_path = data_paths_dict[data_input_path]['reference']
             for i, solution in enumerate(solutions):
 
-                func(data_input_path, solution, join(out_path, i))
+                model_tag = f'model_setup_{i}'
+                func(data_input_path, solution, join(out_path, model_tag), join(ref_path, model_tag))
 
     def test_populate_generators_filter_pointers(self):
         """
@@ -121,17 +147,49 @@ class TestValidation(unittest.TestCase):
         `filter_pointers`. The latter runs on the output of the
         former, so one test is needed to cover both functions.
         """
-        def thingy(input, sol, output):
-            populate_generators(input, sol, output)
-            filter_pointers(input, output)
-            assert True
+        def single_check_populate_generators_and_filter_pointers(input, soln, output, ref):
+            """
+            Runs tests on `populate_generators` and `filter_pointers` for a single model setup.
+            """
+            populate_generators(input, soln, output)
+            read_pandas_frames_and_assert_equal(join(output, 'gen.csv'),
+                                                join(ref, 'gen.csv'))
 
-        self.iterate_func_over_data_and_options(thingy)
+            filter_pointers(input, output)
+            read_pandas_frames_and_assert_equal(join(output, 'timeseries_pointers.csv'),
+                                                join(ref, 'timeseries_pointers.csv'))
+
+        self.iterate_func_over_data_and_options(single_check_populate_generators_and_filter_pointers)
 
     def test_populate_transmission(self):
-        self.iterate_func_over_data_and_options(populate_transmission)
+        """
+        Tests the function `populate_transmission`.
+        """
+        def single_check_populate_transmission(input, soln, output, ref):
+            """
+            Runs tests on `populate_transmission` for a single model setup.
+            """
+            populate_transmission(input, soln, output)
+            read_pandas_frames_and_assert_equal(join(output, 'branch.csv'),
+                                                join(ref, 'branch.csv'))
+        
+        self.iterate_func_over_data_and_options(single_check_populate_transmission)
 
     def test_clone_timeseries(self):
-        self.iterate_func_over_data_and_options(
-            lambda input, _, output: clone_timeseries(input, output)
-        )
+        """
+        Tests the function `clone_timeseries`.
+        """
+        def single_check_clone_timeseries(input, _, output, ref):
+            """
+            Runs tests on `clone_timeseries` for a single model setup.
+            """
+            clone_timeseries(input, output)
+            
+            for file in listdir(input):
+                if file in ['branch.csv', 'gen.csv', 'timeseries_pointers.csv']:
+                    continue # skip the files handled by the other functions
+
+                read_pandas_frames_and_assert_equal(join(output, file),
+                                                    join(ref, file))
+        
+        self.iterate_func_over_data_and_options(single_check_clone_timeseries)
