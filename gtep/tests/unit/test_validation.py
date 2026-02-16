155a22ae8c7ca648d70010e888f03571ca18655c
Allen Moore <agmoore@sandia.gov>
2026-02-16 17:41:58 -0500

    Revert "setting up infrastructure to run tests over multiple model setups"
    This reverts commit 5158de4101a79dfb87f0ff16607b54a5d42196e3.

--- gtep/tests/unit/test_validation.py
+++ gtep/tests/unit/test_validation.py
@@ -31,74 +31,31 @@ from gtep.validation import (
 )
 
 curr_dir = dirname(abspath(__file__))
-
-#############################################################################
-# These tests are designed to run over a collection of models, across both
-# different data sources and model parameters. `data_paths_dict` and
-# `gtep_model_args_list` define the set of collection of models.
-#############################################################################
-
-# maps input path to output and reference paths; potentially include other data sources
-# ...just 5 bus for now.
-data_paths_dict = {
-    abspath(join(curr_dir, "..", "..", "data", "5bus")): {
-        "out": abspath(join(curr_dir, "..", "..", "data", "testing", "5bus", "out")),
-        "ref": abspath(join(curr_dir, "..", "..", "data", "testing", "5bus", "ref")),
-    },
-}
-
-gtep_model_args_list = [
-    {
-        "stages": 2,
-        "num_reps": 2,
-        "len_reps": 1,
-        "num_commit": 6,
-        "num_dispatch": 4,
-    },
-    {
-        "stages": 3,
-        "num_reps": 3,
-        "len_reps": 2,
-        "num_commit": 7,
-        "num_dispatch": 5,
-    },
-]
+input_data_source = abspath(join(curr_dir, "..", "..", "data", "5bus"))
+output_data_source = abspath(join(curr_dir, "..", "..", "data", "5bus_out"))
 
 
-# helper functions
-def get_solution_objects() -> dict[str, list[ExpansionPlanningSolution]]:
-    """
-    :return: Dictionary that maps each data source path to a list of solution objects
-    (each corresponding to a set of arguments to the model constructor)
-    """
+def get_solution_object():
+    data_object = ExpansionPlanningData()
+    data_object.load_prescient(input_data_source)
 
-    solution_objects_dict = {}
+    mod_object = ExpansionPlanningModel(
+        stages=2,
+        data=data_object,
+        num_reps=2,
+        len_reps=1,
+        num_commit=6,
+        num_dispatch=4,
+    )
+    mod_object.create_model()
+    TransformationFactory("gdp.bound_pretransformation").apply_to(mod_object.model)
+    TransformationFactory("gdp.bigm").apply_to(mod_object.model)
+    opt = Highs()
+    mod_object.results = opt.solve(mod_object.model)
 
-    for data_input_path in data_paths_dict:
-        solution_objects_dict[data_input_path] = []
-
-        for gtep_model_args in gtep_model_args_list:
-
-            data_object = ExpansionPlanningData()
-            data_object.load_prescient(data_input_path)
-
-            mod_object = ExpansionPlanningModel(**gtep_model_args, data=data_object)
-            mod_object.create_model()
-
-            TransformationFactory("gdp.bound_pretransformation").apply_to(
-                mod_object.model
-            )
-            TransformationFactory("gdp.bigm").apply_to(mod_object.model)
-
-            opt = Highs()
-            mod_object.results = opt.solve(mod_object.model)
-
-            sol_object = ExpansionPlanningSolution()
-            sol_object.load_from_model(mod_object)
-
-            solution_objects_dict[data_input_path].append(sol_object)
-
-    return solution_objects_dict
+    sol_object = ExpansionPlanningSolution()
+    sol_object.load_from_model(mod_object)
+    return sol_object
 
 
 def read_pandas_frames_and_assert_equal(path_1, path_2):
@@ -127,94 +84,16 @@ class TestValidation(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.solution_dict = get_solution_objects()
-
-    def iterate_func_over_data_and_options(self, func: function):
-        """
-        Loops over every combination of data source and options,
-        calling `func` each time.
-
-        :param func: Must accept four positional arguments:
-            data input path, solution object, data output path, and data output reference path
-        :type func: function
-        """
-
-        for data_input_path, solutions in self.solution_dict.items():
-
-            out_path = data_paths_dict[data_input_path]["out"]
-            ref_path = data_paths_dict[data_input_path]["ref"]
-            for i, solution in enumerate(solutions):
-
-                model_tag = f"model_setup_{i}"
-                func(
-                    data_input_path,
-                    solution,
-                    join(out_path, model_tag),
-                    join(ref_path, model_tag),
-                )
+        cls.solution = get_solution_object()
 
     def test_populate_generators_filter_pointers(self):
-        """
-        Tests the function `populate_generators`, then the function
-        `filter_pointers`. The latter runs on the output of the
-        former, so one test is needed to cover both functions.
-        """
-
-        def single_check_populate_generators_and_filter_pointers(
-            input, soln, output, ref
-        ):
-            """
-            Runs tests on `populate_generators` and `filter_pointers` for a single model setup.
-            """
-
-            populate_generators(input, soln, output)
-            read_pandas_frames_and_assert_equal(
-                join(output, "gen.csv"), join(ref, "gen.csv")
-            )
-
-            filter_pointers(input, output)
-            read_pandas_frames_and_assert_equal(
-                join(output, "timeseries_pointers.csv"),
-                join(ref, "timeseries_pointers.csv"),
-            )
-
-        self.iterate_func_over_data_and_options(
-            single_check_populate_generators_and_filter_pointers
-        )
+        populate_generators(input_data_source, self.solution, output_data_source)
+        # filter_pointers needs to access the gen.csv file created in populate_generators
+        # so these functions need to be tested together
+        filter_pointers(input_data_source, output_data_source)
 
     def test_populate_transmission(self):
-        """
-        Tests the function `populate_transmission`.
-        """
-
-        def single_check_populate_transmission(input, soln, output, ref):
-            """
-            Runs tests on `populate_transmission` for a single model setup.
-            """
-
-            populate_transmission(input, soln, output)
-            read_pandas_frames_and_assert_equal(
-                join(output, "branch.csv"), join(ref, "branch.csv")
-            )
-
-        self.iterate_func_over_data_and_options(single_check_populate_transmission)
+        populate_transmission(input_data_source, self.solution, output_data_source)
 
     def test_clone_timeseries(self):
-        """
-        Tests the function `clone_timeseries`.
-        """
-
-        def single_check_clone_timeseries(input, _, output, ref):
-            """
-            Runs tests on `clone_timeseries` for a single model setup.
-            """
-
-            clone_timeseries(input, output)
-
-            for file in listdir(input):
-                if file in ["branch.csv", "gen.csv", "timeseries_pointers.csv"]:
-                    continue  # skip the files handled by the other functions
-
-                read_pandas_frames_and_assert_equal(join(output, file), join(ref, file))
-
-        self.iterate_func_over_data_and_options(single_check_clone_timeseries)
+        clone_timeseries(input_data_source, output_data_source)
