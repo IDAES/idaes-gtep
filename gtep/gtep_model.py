c28a1af92ced221c82e84405b483e3f922ad9a2c
Edna Soraya Rawlings <esrawli@sandia.gov>
2026-03-18 15:08:45 -0700

    Import transmission logical constraints from separate script

--- gtep/gtep_model.py
+++ gtep/gtep_model.py
@@ -49,6 +49,7 @@ import gtep.model_library.components as comps
 import gtep.model_library.representative_period as rep_period
 import gtep.model_library.scaling as scaling
 import gtep.model_library.storage as stor
+import gtep.model_library.transmission as transm
 
 # Define what a USD is for pyomo units purposes. This will be set to a
 # base year and we will do NPV calculations based on automatic Pyomo
@@ -570,85 +571,4 @@ def model_create_investment_stages(m, stages):
         stor.add_storage_logical_constraints(m)
 
     if m.config["transmission"]:
-        # If a branch is online at time t, it must have been online or installed at time t-1
-        @m.LogicalConstraint(m.stages, m.transmission)
-        def consistent_branch_operation(m, stage, branch):
-            return (
-                m.investmentStage[stage]
-                .branchOperational[branch]
-                .indicator_var.implies(
-                    m.investmentStage[stage - 1].branchOperational[branch].indicator_var
-                    | m.investmentStage[stage - 1].branchInstalled[branch].indicator_var
-                )
-                if stage != 1
-                else pyo.LogicalConstraint.Skip
-            )
-
-        # If a branch is online at time t, it must be online, extended, or retired at time t+1
-        @m.LogicalConstraint(m.stages, m.transmission)
-        def consistent_branch_operation_future(m, stage, branch):
-            return (
-                m.investmentStage[stage - 1]
-                .branchOperational[branch]
-                .indicator_var.implies(
-                    m.investmentStage[stage].branchOperational[branch].indicator_var
-                    | m.investmentStage[stage].branchExtended[branch].indicator_var
-                    | m.investmentStage[stage].branchRetired[branch].indicator_var
-                )
-                if stage != 1
-                else pyo.LogicalConstraint.Skip
-            )
-
-        # Retirement in period t-1 implies disabled in period t
-        @m.LogicalConstraint(m.stages, m.transmission)
-        def full_branch_retirement(m, stage, branch):
-            return (
-                m.investmentStage[stage - 1]
-                .branchRetired[branch]
-                .indicator_var.implies(
-                    m.investmentStage[stage].branchDisabled[branch].indicator_var
-                )
-                if stage != 1
-                else pyo.LogicalConstraint.Skip
-            )
-
-        # If a branch is disabled at time t-1, it must stay disabled or be installed at time t
-        @m.LogicalConstraint(m.stages, m.transmission)
-        def consistent_branch_disabled(m, stage, branch):
-            return (
-                m.investmentStage[stage - 1]
-                .branchDisabled[branch]
-                .indicator_var.implies(
-                    m.investmentStage[stage].branchDisabled[branch].indicator_var
-                    | m.investmentStage[stage].branchInstalled[branch].indicator_var
-                )
-                if stage != 1
-                else pyo.LogicalConstraint.Skip
-            )
-
-        # If a branch is extended at time t-1, it must stay extended or be retired at time t
-        @m.LogicalConstraint(m.stages, m.transmission)
-        def consistent_branch_extended(m, stage, branch):
-            return (
-                m.investmentStage[stage - 1]
-                .branchExtended[branch]
-                .indicator_var.implies(
-                    m.investmentStage[stage].branchExtended[branch].indicator_var
-                    | m.investmentStage[stage].branchRetired[branch].indicator_var
-                )
-                if stage != 1
-                else pyo.LogicalConstraint.Skip
-            )
-
-        # Installation in period t-1 implies operational in period t
-        @m.LogicalConstraint(m.stages, m.transmission)
-        def full_branch_investment(m, stage, branch):
-            return (
-                m.investmentStage[stage - 1]
-                .branchInstalled[branch]
-                .indicator_var.implies(
-                    m.investmentStage[stage].branchOperational[branch].indicator_var
-                )
-                if stage != 1
-                else pyo.LogicalConstraint.Skip
-            )
+        transm.add_transmission_logical_constraints(m)
