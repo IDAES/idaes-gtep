6dc2b2e1e34b96007cadba02749126015d92a9d7
Edna Soraya Rawlings <esrawli@sandia.gov>
2026-04-06 16:14:52 -0700

    Revert changes to scaling since I will include these in a separate PR

--- gtep/model_library/scaling.py
+++ gtep/model_library/scaling.py
@@ -19,29 +19,43 @@ Model
 
 def add_load_scaling(m, b, commitment_period, investment_stage, scaling_value):
 
-    # Loop over the demand at each bus and scale based on the
-    # attributes given in b.
+    # Create a false load directory. [TODO: Commented for now since it
+    # is not used. Check with team if we want to preserve this for
+    # Texas case.]
+    # false_loads = []
+    # for load in m.md.data["elements"]["load"]:
+    #     if type(m.md.data["elements"]["load"][load]) == float:
+    #         false_loads.append(load)
+    # for load in false_loads:
+    #     del m.md.data["elements"]["load"][load]
+    #     # del m.loads[load]
+    # # print(m.loads)
+
+    # Loop over the demand at each bus and scale based on the case.
     for load_n in m.load_buses:
         p_load = m.md.data["elements"]["load"][load_n]["p_load"]["values"][
             commitment_period - 1
         ]
 
-        if hasattr(b, "load_scaling"):
-            print("**Load scaling exists and will be applied to the load parameters")
+        if m.config["scale_loads"]:
+            temp_scale = 10
+            m.loads[load_n] = (
+                p_load
+                * temp_scale
+                * (1 + (temp_scale + investment_stage) / (temp_scale + len(m.stages)))
+            )
 
+        elif m.config["scale_texas_loads"]:
             m.loads[load_n] = (
                 p_load
                 * b.load_scaling[m.md.data["elements"]["load"][load_n]["zone"]].iloc[0]
             )
 
         else:
+            m.loads[load_n] = p_load
 
-            m.loads[load_n] = (
-                p_load
-                * scaling_value
-                * (
-                    1
-                    + (scaling_value + investment_stage)
-                    / (scaling_value + len(m.stages))
-                )
-            )
+        # for key, val in b.loads.items():
+        #     # print(f"{key=}")
+        #     # print(f"{val=}")
+        #     b.loads[key] *= 1/3
+        # print(f"total load at time period = {sum(b.loads.values())}")
