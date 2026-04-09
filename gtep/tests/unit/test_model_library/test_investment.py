d4ebefc929f12c0e6e4641909f94f217e544c640
Belle <bstorm@sandia.gov>
2026-04-09 16:11:19 -0600

    start on tests

--- gtep/tests/unit/test_model_library/test_investment.py
+++ gtep/tests/unit/test_model_library/test_investment.py
@@ -16,6 +16,7 @@ for the investment stage in the Generation and Transmission Expansion Planning (
 model.
 
 """
+import gtep.model_library.investment
 from gtep.model_library.investment import (
     add_investment_params_and_variables,
     add_investment_disjuncts,
@@ -24,12 +25,65 @@ from gtep.model_library.investment import (
 from pyomo.environ import units as u
 import pyomo.environ as pyo
 import pytest
+import pyomo.common.unittest as unittest
+from unittest.mock import patch
+from pint import UnitRegistry
+
+
+# Create a custom pint UnitRegistry with USD and MW defined
+ureg = UnitRegistry()
+ureg.define("USD = [currency]")
+ureg.define("MW = megawatt")
+
+# Create Pyomo units container with this registry
+custom_units = u._PyomoUnitsContainer(ureg)
 
 
 # helper function
-def create_test_inv():
+def create_test_inv(include_commit=True):
     m = pyo.ConcreteModel("GTEP Model")
-    m.stages = 1
+    m.stages = [1]
+    m.config["include_commitment"] = include_commit
+    m.config["storage"] = True
+
+    m.regions = pyo.Set(initialize=["R1", "R2"])
+    m.thermalGenerators = pyo.Set(initialize=["Gen1", "Gen2"])
+
     m.investmentStage = pyo.Block(m.stages)
     inv = m.investmentStage[1]
-    return inv, 1
+    return inv, m
+
+
+# TODO Unit object is throwing an error
+# @patch("gtep.model_library.commitment")
+# def test_investment_params_and_variables(mock_commit_variable):
+#     with patch.object(gtep.model_library.investment, "u", new=custom_units):
+#         b, m = create_test_inv()
+#         add_investment_params_and_variables(b, 1)
+
+#         mock_commit_variable.assert_called_once()
+
+#         # Check parameters
+#         assert hasattr(b, "maxThermalInvestment")
+#         assert hasattr(b, "maxRenewableInvestment")
+#         # Check default values for parameters for each region
+#         for region in m.regions:
+#             assert pyo.value(b.maxThermalInvestment[region]) == 1000
+#             assert pyo.value(b.maxRenewableInvestment[region]) == 1000
+
+#         # Check variables
+#         assert hasattr(b, "quotaDeficit")
+#         assert hasattr(b, "expansionCost")
+#         assert hasattr(b, "storageCostInvestment")
+
+#         # Check variable domains and initial values
+#         assert b.quotaDeficit.domain == pyo.NonNegativeReals
+#         assert pyo.value(b.quotaDeficit) == 0
+#         assert b.expansionCost.domain == pyo.NonNegativeReals
+#         assert pyo.value(b.expansionCost) == 0
+#         assert b.storageCostInvestment.domain == pyo.NonNegativeReals
+#         assert pyo.value(b.storageCostInvestment) == 0
+
+
+def test_add_investment_disjuncts():
+    pass
