8785d9a75892d41a1a7ccb89805eac3e314327ce
Kyle Skolfield <jkskolf@sandia.gov>
2026-05-22 15:16:52 -0600

    something is almost working

--- gtep/model_library/dispatch.py
+++ gtep/model_library/dispatch.py
@@ -226,7 +226,7 @@ def add_dispatch_constraints(b, disp_per):
         def CP_flow_balance(b):
             balance = 0
             buses = [bus for bus in m.buses]
-            loads = [l for l in m.loads]
+            loads = [l for l in c_p.loads]
             gens = [gen for gen in m.generators]
             batts = [bat for bat in m.storage]
             balance += sum(
@@ -240,7 +240,7 @@ def add_dispatch_constraints(b, disp_per):
                 balance += sum(b.storageDischarged[bt] for bt in batts)
                 balance -= sum(b.storageCharged[bt] for bt in batts)
 
-            balance -= sum(m.loads[l] for l in loads)
+            balance -= sum(b.loads[l] for l in loads)
             balance += sum(b.loadShed[bus] for bus in buses)
 
             return balance == 0
@@ -283,7 +283,7 @@ def add_dispatch_constraints(b, disp_per):
             balance -= sum(b.storageCharged[bt] for bt in batts)
 
             # Add the loads as a parameter (already includes units).
-            balance -= m.loads[bus]
+            balance -= c_p.loads[bus]
             balance += b.loadShed[bus]
             return balance == 0 * u.MW
 
