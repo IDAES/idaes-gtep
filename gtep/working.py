57dbb2c917f3ba884324f3aec4b5888d26964d88
Belle <bstorm@sandia.gov>
2026-06-08 11:09:49 -0600

    test resil week data

--- gtep/working.py
+++ gtep/working.py
@@ -1,10 +1,12 @@
 from gtep.gtep_model import ExpansionPlanningModel
 from gtep.gtep_data import ExpansionPlanningData
+import pandas as pd
+import os
 
-
-
-data_path = "./gtep/data/123_Bus_Coal"
+data_path = "./gtep/data/123_Bus_Resil_Week"
 data_object = ExpansionPlanningData(stages=3, num_reps=5, len_reps=24, num_commit=24, num_dispatch=1)
+
+
 data_object.load_prescient(data_path)
 
 load_scaling_path = data_path + "/ERCOT-Adjusted-Forecast.xlsb"
