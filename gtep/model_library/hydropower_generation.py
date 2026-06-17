660edf35b17047c0fb5674c892ffaa0a57562fc1
Edna Soraya Rawlings <esrawli@sandia.gov>
2026-06-17 15:14:29 -0700

    Remove print statement

--- gtep/model_library/hydropower_generation.py
+++ gtep/model_library/hydropower_generation.py
@@ -46,7 +46,6 @@ def fix_hydropower_limits(b, commitmentPeriod):
     units_renewable_capacity = u.MW
 
     for hydroGen in m.hydroGenerators:
-        # print(m.md.data["elements"]["generator"][hydroGen])
         b.hydroCapacityExpected[hydroGen] = (
             m.md.data["elements"]["generator"][hydroGen]["p_max"]["values"][
                 commitmentPeriod - 1
