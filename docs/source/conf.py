b1f9e58f85b0fa51ad5174d7bb3f5b70df86bec1
Gianna Sophia Claros <gsclaro@as421mcslx.sandia.gov>
2026-07-30 14:28:23 -0600

    mbsphinx exec

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,12 +39,22 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx_collections",
     "nbsphinx",
-    "myst_parser"
+    "myst_parser",
+    "pyomo",
+    "numpy",
+    "gridx-egret==0.5.5",
+    "gridx-prescient==2.2.2",
+    "anyio",
+    "pint",
+    "pandas<3",
+    "matplotlib",
+    "ipython",
+    "openpyxl"
 ]
 
 templates_path = ["_templates"]
 exclude_patterns = []
-
+nbsphinx_execute = 'always'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
